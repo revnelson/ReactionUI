10b140d3a968b4796bbb42cc05743d939620852c
Michael Nelson <revnelson@gmail.com>
2019-02-23 23:45:55 +0800

    Reverting last 2 commits.

--- src/shared/pages/about.js
+++ src/shared/pages/about.js
@@ -2,48 +2,39 @@ import React from "react";
 import Helmet from "react-helmet-async";
 import Page from "../lib/page";
 import { Wrapper } from "../style/wrapper";
-import { withApollo } from "react-apollo";
+import { Query } from "react-apollo";
 import { fetchUserQuery } from "../api";
 import { Spin, Alert } from "antd";
 
-const About = async ({ auth, client }) => {
-  if (auth.id) {
-    const { data } = await client.query({ query: fetchUserQuery });
-    spinner = data => {
-      data && (
-        <Spin spinning={!data}>
-          <Alert
-            description="This is the about page"
-            message={
-              !data
-                ? "Loading..."
-                : data.getUser
-                ? `Hello ${data.getUser.username}!`
-                : "Error fetching users"
-            }
-            type="info"
-          />
-        </Spin>
-      );
-    };
-    return (
-      <Wrapper>
-        <Helmet>
-          <title>About Page</title>
-        </Helmet>
-        {data && spinner(data)}
-      </Wrapper>
-    );
-  }
+const About = ({ auth }) => (
+  <Wrapper>
+    <Helmet>
+      <title>About Page</title>
+    </Helmet>
+    {auth.id && (
+      <Query query={fetchUserQuery}>
+        {({ loading, error, data }) => {
+          return (
+            <Spin spinning={!loading}>
+              <Alert
+                description="This is the about page"
+                message={
+                  loading
+                    ? "Loading..."
+                    : data && data.getUser
+                    ? `Hello ${data.getUser.username}!`
+                    : error
+                    ? error
+                    : "Error fetching users"
+                }
+                type="info"
+              />
+            </Spin>
+          );
+        }}
+      </Query>
+    )}
+  </Wrapper>
+);
 
-  return (
-    <Wrapper>
-      <Helmet>
-        <title>About Page</title>
-      </Helmet>
-      <div>Please log in to view this page.</div>
-    </Wrapper>
-  );
-};
-
-export default Page()(withApollo(About));
+export default Page()(About);
