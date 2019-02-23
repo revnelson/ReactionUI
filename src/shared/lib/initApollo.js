ffee30f87eb7e4ddfe00fbc691c5dc4af9743d1d
Michael Nelson <revnelson@gmail.com>
2019-02-24 01:03:53 +0800

    Add debugging console logs to Apollo init.

--- src/shared/lib/initApollo.js
+++ src/shared/lib/initApollo.js
@@ -1,21 +1,61 @@
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { ApolloClient } from "apollo-client";
-import { ApolloLink } from "apollo-link";
+import { ApolloLink, Observable } from "apollo-link";
 import { CachePersistor } from "apollo-cache-persist";
-import { createHttpLink } from "apollo-link-http";
+import { HttpLink } from "apollo-link-http";
+import { onError } from "apollo-link-error";
 import fetch from "isomorphic-unfetch";
 import { clientStore } from "./apolloLinkState";
 
 const { BROWSER_API_URI, SERVER_API_URI, SITE_NAME } = process.env;
 
+const errorMiddleware = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors) {
+    console.log(graphQLErrors);
+  }
+  if (networkError) {
+    console.log(networkError);
+  }
+});
+
+const request = async operation => {
+  console.log(operation);
+};
+
+const requestLink = new ApolloLink(
+  (operation, forward) =>
+    new Observable(observer => {
+      let handle;
+      Promise.resolve(operation)
+        .then(oper => request(oper))
+        .then(() => {
+          handle = forward(operation).subscribe({
+            next: observer.next.bind(observer),
+            error: observer.error.bind(observer),
+            complete: observer.complete.bind(observer)
+          });
+        })
+        .catch(observer.error.bind(observer));
+
+      return () => {
+        if (handle) handle.unsubscribe();
+      };
+    })
+);
+
 export const apolloBrowserInit = async user => {
-  const serverLink = createHttpLink({
+  const serverLink = new HttpLink({
     uri: BROWSER_API_URI,
     credentials: "include",
     fetch
   });
 
-  const link = ApolloLink.from([clientStore(cache), serverLink]);
+  const link = ApolloLink.from([
+    errorMiddleware,
+    requestLink,
+    clientStore(cache),
+    serverLink
+  ]);
 
   const cache = new InMemoryCache();
 
@@ -47,7 +87,7 @@ export const apolloBrowserInit = async user => {
 };
 
 export const apolloServerInit = token => {
-  const serverLink = createHttpLink({
+  const serverLink = new HttpLink({
     uri: SERVER_API_URI,
     headers: {
       Authorization: token ? token : ""
@@ -56,7 +96,12 @@ export const apolloServerInit = token => {
   });
   const cache = new InMemoryCache();
 
-  const link = ApolloLink.from([clientStore(cache), serverLink]);
+  const link = ApolloLink.from([
+    errorMiddleware,
+    requestLink,
+    clientStore(cache),
+    serverLink
+  ]);
 
   return new ApolloClient({ ssrMode: true, link, cache });
 };
